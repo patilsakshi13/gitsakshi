2c36615a91ea19b17fa4bdab2b7314223725bc11
HP <HP@DESKTOP-IU8RUFA>
2023-05-24 15:17:37 +0530

    By SA

--- GitDemoSakshi/src/Com_CJC_GIT/Demo_git.java
+++ GitDemoSakshi/src/Com_CJC_GIT/Demo_git.java
@@ -1,16 +1,16 @@
 package Com_CJC_GIT;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class Demo_git 
 {
-	public void m1()
-	{
-		System.out.println("m1___Demo_git");
-	}
+	
 	public static void main(String[] args) 
 	{
-		System.out.println("Hello Git HUB");
-		System.out.println("Hello Back");
-		System.out.println("Eclips Opend In Git hub");
+		WebDriver driver=new ChromeDriver();
+		System.setProperty("webdriver.chrome.driver", "C:\\Users\\HP\\Desktop\\Seleniunm\\exe\\chromedriver.exe");
+		driver.get("https://github.com/patilsakshi13/gitsakshi/tree/master/GitDemoSakshi");
 		
 	}
 }
